604daba80efa62f11f15b4867d5187288f8123f5
robertobru <roberto.bruschi@unige.it>
2022-11-09 10:40:20 +0100

    blue rest api refactoring

--- blueprints/blue_types.py
+++ blueprints/blue_types.py
@@ -1,21 +1,22 @@
 blueprint_types = {
-
+    "Amari5G": {"class_name": "Amari5G", "module_name": "blue_amari5G"},
+    "Free5Gc": {"class_name": "Free5GC_K8s", "module_name": "blue_free5gc"},
     "K8s": {"class_name": "K8s", "module_name": "blue_k8s"},
-
+    "VO": {"class_name": "VO", "module_name": "blue_vo"},
     "UeRanSim": {"class_name": "UeRanSim", "module_name": "blue_ueransim"},
-
+    "mqtt": {"class_name": "MqttBroker", "module_name": "blue_mqtt"},
+    "Trex": {"class_name": "Trex", "module_name": "blue_trex"},
 }
 
 
 """"
-Amari5G": {"class_name": "Amari5G", "module_name": "blue_amari5G"},
-    "Free5Gc": {"class_name": "Free5GC_K8s", "module_name": "blue_free5gc"},
-    "mqtt": {"class_name": "MqttBroker", "module_name": "blue_mqtt"},
+
+    
     "open5gs": {"class_name": "open5GS", "module_name": "blue_open5gs"},
     "open5gsK8s": {"class_name": "Open5GsK8s", "module_name": "blue_open5gs"},
     "open5gsHybrid": {"class_name": "Open5GS_K8s_ext_upf", "module_name": "blue_open5gs"},
-    "Trex": {"class_name": "Trex", "module_name": "blue_trex"},
-    "VO": {"class_name": "VO", "module_name": "blue_vo"},
+    
+    
     "multi_vyos": {"class_name": "VyOsMultipleTunnels", "module_name": "blue_vyos"},
     "Vyos": {"class_name": "VyOSBlue", "module_name": "blue_vyos"}
     """

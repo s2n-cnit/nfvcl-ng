50219ed6512181597e91fba076b14fdfc8808a06
Guerino <guerino.lamanna@infocomgenova.it>
2022-11-18 16:46:27 +0100

    - add "Free5gck8sBlueCreateModel" to blue models

--- rest_endpoints/blue_models.py
+++ rest_endpoints/blue_models.py
@@ -1,5 +1,6 @@
 from blueprints.blue_k8s.models import K8sBlueprintCreate, K8sBlueprintScale
 from blueprints.blue_5g_base.models import Create5gModel
+from blueprints.blue_free5gc.models import Free5gck8sBlueCreateModel
 from blueprints.blue_mqtt.models import MqttRequestBlueprintInstance
 from blueprints.blue_trex.models import TrexRequestBlueprintInstance
 from blueprints.blue_ueransim.models import UeranSimBlueprintRequestInstance
@@ -9,6 +10,7 @@ from typing import Union
 blue_create_models = Union[
     K8sBlueprintCreate,
     Create5gModel,
+    Free5gck8sBlueCreateModel,
     MqttRequestBlueprintInstance,
     TrexRequestBlueprintInstance,
     UeranSimBlueprintRequestInstance,

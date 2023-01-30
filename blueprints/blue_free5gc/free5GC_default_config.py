0c59a56890dc20b5eca946b37c87270921bb7c63
Guerino <guerino.lamanna@infocomgenova.it>
2023-01-30 09:44:43 +0100

    - Free5GC:   - default config changes (SMF)   - UPF playbook bug fixing

--- blueprints/blue_free5gc/free5GC_default_config.py
+++ blueprints/blue_free5gc/free5GC_default_config.py
@@ -1203,34 +1203,6 @@ default_config = {
                 ]
               }
             ],
-          },
-          "UE2": { # Group Name
-            "members": [
-              "imsi-208930000000004" # Subscription Permanent Identifier of the UE
-            ],
-            "topology": [ # Network topology for this group (Uplink: A->B, Downlink: B->A)
-            # default path derived from this topology
-            # node name should be consistent with smfcfg.yaml
-              {
-                "A": "gNB1",
-                "B": "BranchingUPF"
-              },
-              {
-                "A": "BranchingUPF",
-                "B": "AnchorUPF1"
-              }
-            ],
-            "specificPath": [
-              {
-                "dest": "10.100.100.16/32", # the destination IP address on Data Network (DN)
-                # the order of UPF nodes in this path. We use the UPF's name to represent each UPF node.
-                # The UPF's name should be consistent with smfcfg.yaml
-                "path": [
-                  "BranchingUPF",
-                  "AnchorUPF2"
-                ]
-              }
-            ]
           }
         },
         "logger": {

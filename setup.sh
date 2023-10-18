ba72445928567980372c7f591c72e64677cf7734
PaoloB98 <paolobono98@gmail.com>
2023-10-18 15:09:00 +0200

    Fixing setup installation

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg --dearmo
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
 echo "Adding Python 3.11 repo"
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update
 
 echo "Installing python3.11"
-sudo apt install python3.11
+sudo apt install -y python3.11 python3.11-dev python3.11-venv python3.11-distutils
 
 echo "Installing Poetry"
 curl -sSL https://install.python-poetry.org | python3 -
@@ -21,10 +21,6 @@ sudo apt install -y redis-server
 sudo sed -i 's/bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis/redis.conf
 echo "Restarting redis..."
 sudo systemctl restart redis
-
-echo "Installing python3.11..."
-sudo apt install python3.11
-sudo apt install python3.11-dev
 
 echo "Install MongoDB..."
 sudo apt install -y mongodb-org
@@ -39,8 +35,11 @@ echo "Fixing PodSecurity issue for VNFM"
 kubectl set env deployment -n osm lcm OSMLCM_VCA_EEGRPC_POD_ADMISSION_POLICY=privileged
 
 echo "Setting up the registry for local image deployment"
-docker run -d -p 5000:5000 --restart=always --name registry registry:2
+sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2
 echo "Building VNFM image"
 # The build should take enough time to let the registry start
-docker build -t localhost:5000/vnfm-ee ./vnf_managers/helmflexvnfm --no-cache
-docker push localhost:5000/vnfm-ee
+sudo docker build -t localhost:5000/vnfm-ee ./vnf_managers/helmflexvnfm --no-cache
+sudo docker push localhost:5000/vnfm-ee
+
+echo "Installing Python project dependencies"
+/home/ubuntu/.local/bin/poetry install

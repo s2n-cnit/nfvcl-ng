9a18d31362a6e2754669786a2e60589c569a1b6a
PaoloB98 <paolobono98@gmail.com>
2024-01-10 15:43:06 +0100

    fix(Openstack utils): check net was returning always true if error did not occur.

--- utils/openstack/openstack_utils.py
+++ utils/openstack/openstack_utils.py
@@ -5,7 +5,7 @@ from openstack.network.v2.network import Network
 from config_templates.openstack.image_manager import get_nfvcl_image_list
 from models.openstack.images import ImageRepo
 from models.vim import VimModel
-from topology.topology import Topology, build_topology
+from topology.topology import build_topology
 from utils.log import create_logger
 from utils.openstack.openstack_client import OpenStackClient
 
@@ -75,9 +75,9 @@ def check_images(vim: VimModel) -> bool:
 
 def check_networks(vim: VimModel) -> bool:
     """
-
+    Checks that all networks declared to be part of a VIM are existing on the VIM.
     Args:
-        vim: The vim to be checked
+        vim: The vim to be checked (a network list is included in the object)
 
     Returns:
         True if the VIM
@@ -92,17 +92,19 @@ def check_networks(vim: VimModel) -> bool:
         for network_name in vim.networks:
             # All net details are found in the topology
             network_to_check = topology.get_network(network_name) # get info from topology
-            # It looks for the network
+            # It looks for the network in the openstack list
             match_net_on_openstack = [os_net for os_net in openstack_network_list if os_net.name == network_name]
 
             # TODO check that network cidr, dns, gateway... are the same (topology info vs openstack)
 
+            # If not found len == 0
             if len(match_net_on_openstack) <= 0:
                 logger.warning(f"The network {network_to_check} does NOT exist in the OpenStack server {vim.name}")
-
+                return False
     except Unauthorized as e:
         logger.error(f"The VIM {vim.name} is not working correctly. Error: \n{e}")
         return False
 
-    # If ???
+    # If all the networks are present
+    logger.debug(f"Nets are present in OpenStack server {vim.name}")
     return True

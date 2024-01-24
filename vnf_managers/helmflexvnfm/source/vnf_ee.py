e8b26733a71eb6e82182e708deb17ed7073a2a0c
PaoloB98 <paolobono98@gmail.com>
2024-01-24 10:12:14 +0100

    feat(VNFM): Started upgrading the VNFM
    First upgrade for the VNFM, trying to realize better output and logging.

--- vnf_managers/helmflexvnfm/source/vnf_ee.py
+++ vnf_managers/helmflexvnfm/source/vnf_ee.py
@@ -25,32 +25,140 @@ import requests
 import traceback
 import paramiko
 import time
-
+import logging
+from logging.handlers import RotatingFileHandler
+import coloredlogs
+import verboselogs
 
 from osm_ee.exceptions import VnfException
-
 import osm_ee.util.util_ee as util_ee
 #import osm_ee.util.util_ansible as util_ansible
 
+############## LOGGER #################
+#######################################
+_log_level = logging.DEBUG
+
+
+def set_log_level(level):
+    """
+    Set the level of the loggers that will be created. Old loggers will have the old value.
+    Args:
+        level: the level to be set
+    """
+    global _log_level
+    _log_level = level
+
+
+coloredlog_format_string = "%(asctime)s [%(name)-20.20s][%(threadName)-10.10s] [%(levelname)8s] %(message)s"
+
+level_styles = {
+    'spam': {'color': 'green', 'faint': True},
+    'debug': {'color': 241},
+    'verbose': {'color': 'blue'},
+    'info': {},
+    'notice': {'color': 'magenta'},
+    'warning': {'color': 'yellow'},
+    'success': {'color': 'green', 'bold': True},
+    'error': {'color': 'red'},
+    'critical': {'color': 'red', 'bold': True}
+}
+
+field_styles = {
+    'asctime': {'color': 247},
+    'hostname': {'color': 'magenta'},
+    'levelname': {'color': 'cyan', 'bold': True},
+    'name': {'color': 33},
+    'programname': {'color': 'cyan'},
+    'username': {'color': 'yellow'},
+    'devicename': {'color': 34}
+}
+coloredlog_formatter = coloredlogs.ColoredFormatter(
+    fmt=coloredlog_format_string,
+    field_styles=field_styles,
+    level_styles=level_styles
+)
+
+ROOT_LOGGER_NAME = "RootLogger"
+formatter = logging.Formatter(coloredlog_format_string)
+
+
+def create_logger(name: str, ov_log_level: int = None) -> logging.Logger:
+    """
+    Creates a logger outputting on: console, redis, and on file.
+    In this way, an external entity to the NFVCL is able to observe what is going on.
+    The log file allows permanent info in case of failure (NB on next restart the log file is overwritten)
+
+    Args:
+        name: The name of the logger to be displayed in logs.
+        ov_log_level: Can be used to override global log level
+
+    Returns:
+
+        The created logger
+    """
+    # If defined use override log level, otherwise the global.
+    if ov_log_level is not None:
+        local_log_level = ov_log_level
+    else:
+        local_log_level = _log_level
+
+    logger = verboselogs.VerboseLogger(name)
+    logger.parent = logging.getLogger(ROOT_LOGGER_NAME)
+
+    # Adding file handler to post log into file
+    # w = every restart log is cleaned
+    log_file_handler = RotatingFileHandler("nfvmanager.log", maxBytes=10000000, backupCount=4)
+    log_file_handler.setLevel(local_log_level)
+    log_file_handler.setFormatter(formatter)
+    logger.addHandler(log_file_handler)
+
+    coloredlogs.install(
+        level=local_log_level,
+        logger=logger,
+        fmt=coloredlog_format_string,
+        field_styles=field_styles,
+        level_styles=level_styles
+    )
+
+    return logger
+
+def mod_logger(logger: logging.Logger):
+    """
+    This method takes an existing logger and mod it.
+    """
+    coloredlogs.install(
+        level=_log_level,
+        logger=logger,
+        fmt=coloredlog_format_string,
+        field_styles=field_styles,
+        level_styles=level_styles
+    )
+
+global_logger = create_logger("VNF-Manager", logging.DEBUG)
+
+#############VNF MANAGER ####################
+#############################################
+
 def wait_for_ssh_to_be_ready(host: str, port: int, user: str, passwd: str, timeout: int, retry_interval: float) -> bool:
+    global_logger.debug(f"Starting SSH connection to {host}:{port} as user <{user}> and passwd <{passwd}>. Timeout is {timeout}, retry interval is {retry_interval}")
     client = paramiko.client.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     timeout_start = time.time()
     while time.time() < timeout_start + timeout:
         try:
             client.connect(host, port, username=user, password=passwd, allow_agent=False, look_for_keys=False)
-            print('SSH transport is available!')
+            global_logger.debug('SSH transport is available!')
             client.close()
             return True
         except paramiko.ssh_exception.SSHException as e:
             # socket is open, but not SSH service responded
-            print("socket is open, but not SSH service responded")
-            print(e)
+            global_logger.debug("socket is open, but not SSH service responded")
+            global_logger.debug(e)
             time.sleep(retry_interval)
             continue
 
         except paramiko.ssh_exception.NoValidConnectionsError as e:
-            print('SSH transport is not ready...')
+            global_logger.debug('SSH transport is not ready...')
             time.sleep(retry_interval)
             continue
     return False
@@ -63,7 +171,7 @@ class VnfEE:
     ANSIBLE_INVENTORY_PATH = "/etc/ansible/hosts"
 
     def __init__(self, config_params):
-        self.logger = logging.getLogger('osm_ee.vnf')
+        self.logger = global_logger
         self.config_params = config_params
 
     async def config(self, id_, params):
@@ -134,6 +242,7 @@ class VnfEE:
                     self.config_params["ssh-password"],
                     300,
                     5):
+                self.logger.error("Cannot establish connection to ssh server: {}".format(self.config_params["ssh-hostname"]))
                 raise ValueError('SSH connection not available')
 
             self.logger.debug('Applying configurations and commands')

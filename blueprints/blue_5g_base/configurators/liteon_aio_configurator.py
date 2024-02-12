1610af2d934e2205cb2c3a30aaf14cd5009053fd
Alderico Gallo <alderico.gallo@gmail.com>
2024-02-12 10:46:09 +0100

    feat(LiteON): Added frequency configuration
    Made other configs optional, now these can be set in the PDU config field

--- blueprints/blue_5g_base/configurators/liteon_aio_configurator.py
+++ blueprints/blue_5g_base/configurators/liteon_aio_configurator.py
@@ -15,24 +15,29 @@ class Configurator_LiteON_AIO(Configurator_Flex):
         self.type = "liteonaio"
         super(Configurator_LiteON_AIO, self).__init__(nsd_id, m_id, blue_id)
         logger.info("Configurator_LiteON_AIO allocated")
-        self.day2_conf_file_name = f"liteon_aio_tac_{args['tac']}_plmn_{args['plmn']}_blue_{blue_id}.conf"
+        self.day2_conf_file_name = f"liteon_aio_blue_{blue_id}.conf"
+        # self.day2_conf_file_name = f"liteon_aio_tac_{args['tac']}_plmn_{args['plmn']}_blue_{blue_id}.conf"
 
         self.conf = pdu['config']  # we start with the config data saved in the db
+        if "plmn" in args:
+            self.conf['mcc'] = args['plmn'][:3]
+            self.conf['mnc'] = args['plmn'][3:]
 
-        self.conf['mcc'] = args['plmn'][:3]
-        self.conf['mnc'] = args['plmn'][3:]
+        if "tac" in args:
+            self.conf['gnbid'] = args['tac']
+            self.conf['tac'] = args['tac']
+            self.conf['nci'] = args['tac']
 
-        self.conf['gnbid'] = args['tac']
-        self.conf['tac'] = args['tac']
-        self.conf['nci'] = args['tac']
-
-        self.conf['pci'] = 1  # TODO
+        if "pci" in args:
+            self.conf['pci'] = args["pci"]
 
         self.conf['sst'] = args['sst']
         self.conf['sd'] = args['sd']
 
-        self.conf['amf_ip'] = args['amf_ip']
-        self.conf['upf_ip'] = args['upf_ip']
+        if "amf_ip" in args:
+            self.conf['amf_ip'] = args['amf_ip']
+        if "upf_ip" in args:
+            self.conf['upf_ip'] = args['upf_ip']
 
         self.addPlaybook('blueprints/blue_5g_base/config_scripts/liteon_playbook.yaml', vars_=self.conf)
 

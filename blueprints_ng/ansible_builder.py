59e7a8b90e52f5a6815f771618a7677a7e6b4c29
Alderico Gallo <alderico.gallo@gmail.com>
2024-02-27 09:29:19 +0100

    docs(BlueprintNG): Added documentation for Ansible Playbook Builder

--- blueprints_ng/ansible_builder.py
+++ blueprints_ng/ansible_builder.py
@@ -10,6 +10,10 @@ from models.base_model import NFVCLBaseModel
 
 
 class MyYAML(YAML):
+    """
+    Custom override of the YAML class to allow the dump method to return a string instead of writing to file
+    """
+
     def __init__(self: Any, *, typ: Optional[Union[List[Text], Text]] = None, pure: Any = False, output: Any = None, plug_ins: Any = None) -> None:
         super().__init__(typ=typ, pure=pure, output=output, plug_ins=plug_ins)
         self.preserve_quotes = True
@@ -60,6 +64,13 @@ class AnsibleTemplateTask(AnsibleTask):
 
 class AnsiblePlaybookBuilder:
     def __init__(self, name, become=True, gather_facts=False):
+        """
+        Create a new Ansible Playbook builder
+        Args:
+            name: Name of the playbook
+            become: Escalate privileges, Default to True
+            gather_facts: Gather facts, see https://docs.ansible.com/ansible/latest/playbook_guide/playbooks_vars_facts.html require Python on the remote machine
+        """
         self.name = name
         self.playbook = AnsiblePlaybook(
             name=self.name,
@@ -71,15 +82,41 @@ class AnsiblePlaybookBuilder:
         )
 
     def set_vars(self, vars_: Dict[str, Any]):
+        """
+        Set the variables for the playbook, this will override existing vars
+        Args:
+            vars_: Dict of variables
+        """
         self.playbook.vars = vars_
 
     def set_var(self, key: str, value: Any):
+        """
+        Set a single variable in the playbook
+        Args:
+            key: Key of the variable
+            value: Value of the variable
+        """
         self.playbook.vars[key] = value
 
     def unset_var(self, key: str):
-        del self.playbook.vars[key]
+        """
+        Delete a variable from the playbook
+        Args:
+            key: Key of the variable to delete
+        Returns:
+            True if the variable is present and deleted, False if not present
+        """
+        if key in self.playbook.vars:
+            del self.playbook.vars[key]
+            return True
+        return False
 
     def add_tasks_from_file(self, playbook_file: Path):
+        """
+        Add every task from a playbook file, only add tasks from the first playbook in the file
+        Args:
+            playbook_file: Path of the playbook file
+        """
         with open(playbook_file, 'r+') as stream_:
             plays_ = yaml.load(stream_)
             for task in plays_[0]["tasks"]:
@@ -94,12 +131,26 @@ class AnsiblePlaybookBuilder:
             self.playbook.tasks.append(task)
 
     def add_task(self, name: str, task_module: str, task_content: AnsibleTask):
+        """
+        Add a single task to the playbook
+        Args:
+            name: Name of the task
+            task_module: Ansible module of the task
+            task_content: Content of the task
+        """
         self.playbook.tasks.append({
             "name": name,
             task_module: task_content
         })
 
     def add_template_task(self, src: Path, dest: str):
+        """
+        Add a task of the 'ansible.builtin.template' type, this will copy the file at the src path on the NFVCL server to the dest on the remote machine
+        This task will also resolve every jinja2 template in the file using the playbook vars, internal Ansible vars can also be used
+        Args:
+            src: Path of the source file
+            dest: Remote destination
+        """
         self.add_task(
             f"Template task for {dest}",
             "ansible.builtin.template",
@@ -110,4 +161,8 @@ class AnsiblePlaybookBuilder:
         )
 
     def build(self) -> str:
+        """
+        Build the playbook and return it as a yaml string
+        Returns: YAML string of the playbook
+        """
         return yaml.dump([self.playbook.model_dump()])

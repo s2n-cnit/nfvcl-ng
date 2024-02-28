3b5c65f4523d3e75e55acc1349dfca0eee54c130
PaoloB98 <paolobono98@gmail.com>
2024-02-28 15:21:07 +0100

    fix(HelmChart): AOI empty helm-chart and added patterns

---
+++ utils/patterns.py
@@ -0,0 +1,11 @@
+class Singleton(type):
+    __unique_instance = None
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Override the __call__ method to make sure only one instance is created.
+        """
+        if cls.__unique_instance is None:
+            cls.__instance = type.__call__(cls, *args, **kwargs)
+
+        return cls.__instance

f9eacc371b263a1d234a34e17a25afb9dbb5f265
PaoloB98 <paolobono98@gmail.com>
2024-03-27 15:00:23 +0100

    feat(K8SBlueNG): Configurators
    Created configurator to remove nodes. Prepared Karmada installation

--- blueprints_ng/modules/k8s/config/day0_configurator.py
+++ blueprints_ng/modules/k8s/config/day0_configurator.py
@@ -3,6 +3,7 @@ from ruamel.yaml.scalarstring import LiteralScalarString
 from blueprints_ng.utils import rel_path
 from blueprints_ng.ansible_builder import AnsiblePlaybookBuilder
 from blueprints_ng.resources import VmResourceAnsibleConfiguration
+from models.blueprint_ng.k8s.k8s_rest_models import KarmadaInstallModel
 
 
 class VmK8sDay0Configurator(VmResourceAnsibleConfiguration):
@@ -21,7 +22,7 @@ class VmK8sDay0Configurator(VmResourceAnsibleConfiguration):
     def configure_master(self, master_ip, master_external_ip, pod_network_cidr, k8s_service_cidr):
         self._ansible_builder = AnsiblePlaybookBuilder("K8s Master Day0Configurator")
         # Using the playbook to configure the master
-        self._ansible_builder.add_tasks_from_file(rel_path("playbooks/playbook_kubernetes_master.yaml"))
+        self._ansible_builder.add_tasks_from_file(rel_path("playbooks/kubernetes_master_day0.yaml"))
 
         # Set the playbook variables for the master node
         self._ansible_builder.set_var("pod_network_cidr", pod_network_cidr)
@@ -36,8 +37,20 @@ class VmK8sDay0Configurator(VmResourceAnsibleConfiguration):
 
     def configure_worker(self, join_command, credentials_file):
         self._ansible_builder = AnsiblePlaybookBuilder("K8s Worker Day0Configurator")
-        self._ansible_builder.add_tasks_from_file(rel_path("playbooks/playbook_kubernetes_worker.yaml"))
+        self._ansible_builder.add_tasks_from_file(rel_path("playbooks/kubernetes_worker_day0.yaml"))
         # Set the playbook variables
         self._ansible_builder.set_var("join_command", join_command) # The command to join the cluster
         self._ansible_builder.set_var("credentials_file", LiteralScalarString(textwrap.dedent(credentials_file))) # Credential file to be used for k8s management in workers
         self._ansible_builder.add_gather_var_task("joined_or_not")
+
+    def install_karmada(self, request: KarmadaInstallModel):
+        self._ansible_builder = AnsiblePlaybookBuilder("K8s Master Day0 Karmada")
+        # Using the playbook to configure karmada and submariner
+        self._ansible_builder.add_tasks_from_file(rel_path("playbooks/kubernetes_master_inst_karmada.yaml"))
+        # Set the playbook variables for karmada, submariner
+        self._ansible_builder.set_var("cluster_id", request.cluster_id)
+        self._ansible_builder.set_var("kube_config_location", request.kube_config_location)
+        self._ansible_builder.set_var("submariner_broker", request.submariner_broker)
+        self._ansible_builder.set_var("karmada_control_plane", request.karmada_control_plane)
+        self._ansible_builder.set_var("karmada_token", request.karmada_token)
+        self._ansible_builder.set_var("discovery_token_hash", request.discovery_token_hash)

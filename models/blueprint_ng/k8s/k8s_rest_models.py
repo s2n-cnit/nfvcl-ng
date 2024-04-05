62d12806b63b451010120af2701a2f5b19787b2d
PaoloB98 <paolobono98@gmail.com>
2024-04-05 14:04:25 +0200

    feat(K8s models): New models and functionalities

--- models/blueprint_ng/k8s/k8s_rest_models.py
+++ models/blueprint_ng/k8s/k8s_rest_models.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
-from typing import List
+from typing import List, Optional
 from models.k8s.common_k8s_model import Cni
 from blueprints_ng.resources import VmResourceFlavor
-from pydantic import Field, field_validator, PositiveInt
+from pydantic import Field, field_validator, PositiveInt, model_validator, ValidationError
 from blueprints.blue_vyos import VyOSSourceNATRule
 from blueprints_ng.blueprint_ng import BlueprintNGCreateModel
 from models.base_model import NFVCLBaseModel
@@ -12,8 +12,8 @@ class K8sAreaDeployment(NFVCLBaseModel):
     area_id: int = Field(description="The area in witch the deployment is made")
     is_master_area: bool = Field(default=False, description="If true the master is deployed in this area")
     mgmt_net: str = Field(description="The management network") # TODO validate
-    service_net: str = Field(min_items=1, description="Network on witch services are exposed") # TODO validate
-    service_net_required_ip_number: int = Field(default=20, description="The required IPs number to exposed services, they should be reserved")
+    service_net: Optional[str] = Field(default=None, min_items=1, description="Network on witch services are exposed. If not present, service are exposed to an internal net (that can be accessed thought a VXLAN tunnel)")
+    service_net_required_ip_number: int = Field(default=20, description="The required IPs number to exposed services, they should be reserved. USED only in master area")
     worker_replicas: PositiveInt = Field(default=1, description="Number of workers in this area")
     worker_flavors: VmResourceFlavor = VmResourceFlavor(memory_mb="8192", storage_gb='32', vcpu_count='6')
 
@@ -24,6 +24,19 @@ class K8sAreaDeployment(NFVCLBaseModel):
         if isinstance(other, K8sAreaDeployment):
             return self.area_id == other.area_id
         return False
+
+    @model_validator(mode='after')
+    def check_service_net(self) -> K8sAreaDeployment:
+        """
+        Checks if the service net is present, and it is the master area. We choose to expose services only in master area (where the controller is located)
+        """
+        if not self.service_net:
+            return self
+        if self.service_net:
+            # TODO check if net exist
+            if self.is_master_area == False:
+                raise ValidationError("Service net should be only present in master area")
+            return self
 
 class K8sCreateModel(BlueprintNGCreateModel):
     """
@@ -50,6 +63,16 @@ class K8sCreateModel(BlueprintNGCreateModel):
 
         return areas
 
+    def get_master_area(self) -> K8sAreaDeployment | None:
+        """
+        Returns the master area of the request
+        """
+        master_areas = [area for area in self.areas if area.is_master_area == True]
+        if len(master_areas) > 0:
+            return master_areas[0]
+        else:
+            return None
+
 
 class K8sAddNodeModel(BlueprintNGCreateModel):
     areas: List[K8sAreaDeployment] = Field(min_items=1, description="List of areas in witch deployment is made (with requirements)")
@@ -57,6 +80,9 @@ class K8sAddNodeModel(BlueprintNGCreateModel):
     @field_validator('areas')
     @classmethod
     def check_areas(cls, areas: List[K8sAreaDeployment]) -> List[K8sAreaDeployment]:
+        """
+        Checks if there is NOT a master area in day2 call
+        """
         if isinstance(areas, list):
             if len([area for area in areas if area.is_master_area == True]) >= 1:
                 raise ValueError("It is not possible to add CORE areas in day2 request. Only upon cluster creation.")
@@ -76,3 +102,15 @@ class KarmadaInstallModel(BlueprintNGCreateModel):
     karmada_control_plane: str = Field(description="The IP and port of Karmada")
     karmada_token: str = Field(description="The Karmada token")
     discovery_token_hash: str = Field(description="Discovery token hash")
+
+class CreateVxLanModel(NFVCLBaseModel):
+    """
+    Model used to request a VxLan tunnel to interact with internal k8s networks if needed.
+    """
+    vx_name: str = Field(description="The name of the VxLan")
+    vxid: str = Field(description="The VxLan ID")
+    vx_client_ip: str = Field(description="The remote IP that want to be connected with the K8S internal network")
+    vx_server_ip: Optional[str] = Field(default=None, description="The local IP on the K8S side where the VxLAN is listening")
+    vx_server_ext_device: Optional[str] = Field(default=None, description="The name of the interface where the VxLan is listening")
+    vx_server_ext_port: str = Field(default="4789")
+    vxlan_server_int_cidr: str = Field(description="The CIDR of the VxLan, e.g. 10.234.154.0/24")
--- models/k8s/common_k8s_model.py
+++ models/k8s/common_k8s_model.py
@@ -1,8 +1,7 @@
 from enum import Enum
-from typing import Optional
-
+from ipaddress import IPv4Address
+from typing import Optional, List
 from pydantic import Field
-
 from models.base_model import NFVCLBaseModel
 
 
@@ -27,6 +26,27 @@ class LBPool(NFVCLBaseModel):
     ip_end: Optional[str] = Field(default=None)
     range_length: Optional[int] = Field(None,description='Number of IPv4 addresses to reserved if no ip start'
                                                          ' and end are passed. Default 10 addresses.')
+
+    def get_ip_address_list(self, max_num: int = 256) -> List[str]:
+        """
+        Retrieve a list of IP addresses in the pool. It is possible to limit the number of IPs using the function parameter
+        Args:
+            max_num: The maximum number of IPs retrieved in the list
+
+        Returns:
+            A list of IP addresses in the list
+        """
+        ip_list: List[str] = []
+        ip_s = IPv4Address(self.ip_start)
+        ip_e = IPv4Address(self.ip_end)
+
+        for i in range(max_num):
+            ip_list.append((ip_s+i).exploded)
+            if ip_s+i == ip_e:
+                break
+
+        assert len(ip_list)>0
+        return ip_list
 
     class Config:
         use_enum_values = True

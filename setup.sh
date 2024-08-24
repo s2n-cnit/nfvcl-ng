26f6b6bf6133142214449efb9fc1d4d3205bbd18
Paolo <paolob@tnt-lab.unige.it>
2024-08-24 12:16:20 +0200

    fix(General): Setup
    Helm client installation was missing from setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,13 @@ sudo mkdir /etc/ansible
 sudo touch /etc/ansible/ansible.cfg
 echo -e '[defaults]\nhost_key_checking = False' | sudo tee -a /etc/ansible/ansible.cfg
 
+echo "Installing Helm"
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
 echo "Installing Poetry"
 curl -sSL https://install.python-poetry.org | python3 -
 

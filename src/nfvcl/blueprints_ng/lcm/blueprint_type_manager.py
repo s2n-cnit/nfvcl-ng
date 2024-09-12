926f57d0e8f1991dc41379bc8241560519ba7bc4
Alderico Gallo <alderico.gallo@gmail.com>
2024-09-12 12:04:37 +0200

    fix(BlueprintNG): Fixed warning for day2 GET arguments number

--- src/nfvcl/blueprints_ng/lcm/blueprint_type_manager.py
+++ src/nfvcl/blueprints_ng/lcm/blueprint_type_manager.py
@@ -95,7 +95,7 @@ class blueprint_type:
             raise ValueError(f"Duplicate path found. The route {final_path} cannot be added since is already pointing function {method.__qualname__}")
 
         param_num = len(signature(method).parameters)
-        if param_num != 2:
+        if request_type[0] != HttpRequestType.GET and param_num != 2:
             logger.warning(f"Day2 function must have 2 arguments. {method.__qualname__} has {param_num}")
 
         # Recreate the complete endpoint path that is given by type + final path. E.g., 'vyos'+'/test_api_path' --> 'vyos/test_api_path'
